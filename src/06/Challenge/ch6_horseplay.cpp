e887b94c0720b582d304dbfe0100f2de1d9e9d1a
Ajith D <67494493+mr-speedster@users.noreply.github.com>
2023-11-18 18:46:40 +0000

    knight move

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -17,7 +17,37 @@
 // Returns: An STL vector of strings with the possible locations to move.
 std::vector<std::string> knight_moves(std::string knight){
     std::vector<std::string> moves;
+    int x, y;
+    x = knight[0] - 'a' + 1;
+    y = knight[1] - '0';
+    const int delta1[2] = {-1, 1};
+    const int delta2[2] = {-2, 2};
+    
+    int new_x;
+    int new_y;
+    std::string new_loc = "  ";
 
+    for(int i = 0; i < 2; i++)
+        for(int j = 0; j < 2; j++){
+            new_x = x + delta1[i];
+            new_y = y + delta2[j];
+            
+            if(new_x > 0 && new_x < 9 && new_y > 0 && new_y < 9 ){
+                new_loc[0] = new_x + 'a' - 1;
+                new_loc[1] = new_y + '0';
+                moves.push_back(new_loc);
+            }
+
+            new_x = x + delta2[i];
+            new_y = y + delta1[j];
+            
+            if(new_x > 0 && new_x < 9 && new_y > 0 && new_y < 9 ){
+                new_loc[0] = new_x + 'a' - 1;
+                new_loc[1] = new_y + '0';
+                moves.push_back(new_loc);
+            }
+        }
+    return moves;
     // Write your code here
 
     return moves;
